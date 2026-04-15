d7c6be07b1ff89e4e6f5067ebda12c77645f280b
michal <mmasalski@student.agh.edu.pl>
2026-04-15 19:25:21 +0200

    uporządkowano pliki, dodano nowy projekt

--- Scanner/Main.java
+++ Scanner/Main.java
@@ -1,3 +1,5 @@
+package Scanner;
+
 import java.util.Scanner;
 
 class Main {
--- Scanner/Skaner.java
+++ Scanner/Skaner.java
@@ -1,3 +1,5 @@
+package Scanner;
+
 public class Skaner {
     enum KodTokena {
         NUM, ID, PLUS, MINUS, MULT, DIV, LBRACKET, RBRACKET, ERR, EOF
